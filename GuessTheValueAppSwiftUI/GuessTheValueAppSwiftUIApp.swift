66f1d37ed5611689b7aef6c7c807762c48200dea
Aleksandra Moiseenko <lesovskaya.aleks@mail.ru>
2022-05-23 09:55:21 +0300

    Initial Commit

---
+++ GuessTheValueAppSwiftUI/GuessTheValueAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheValueAppSwiftUIApp.swift
+//  GuessTheValueAppSwiftUI
+//
+//  Created by Александра Лесовская on 23.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheValueAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
